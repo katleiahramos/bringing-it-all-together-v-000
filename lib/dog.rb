d2814ac181e0b236c26d9ac77cdf0ea6504796d0
Katleiah Ramos <katleiahramos@gmail.com>
2018-05-11 15:17:30 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -90,8 +90,10 @@ class Dog
       WHERE name = ?
     SQL
 
-    DB[:conn].execute(sql, name)
-  end 
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end 
+  end
 
 
 end
